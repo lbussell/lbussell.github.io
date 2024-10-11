efeeb92397b356824c7b548abb9b4b646b8ee170
Logan Bussell <loganbussell@microsoft.com>
2024-10-11 15:17:03 -0400

    Set up editorconfig

--- src/SiteGenerator/Program.cs
+++ src/SiteGenerator/Program.cs
@@ -1,9 +1,18 @@
 ﻿namespace SiteGenerator;
 
-public class Program
+internal class Program
 {
-    public static void Main(string[] args)
+    public static void Main(string content, string? output)
     {
         Console.WriteLine("Hello, World!");
+        ValidatePathArg(content, nameof(content));
+    }
+
+    private static void ValidatePathArg(string path, string argName)
+    {
+        if (!Directory.Exists(path))
+        {
+            throw new DirectoryNotFoundException($"The {argName} directory '{path}' does not exist.");
+        }
     }
 }
